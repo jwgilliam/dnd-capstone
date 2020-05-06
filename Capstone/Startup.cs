1f27f55da0c0b9532564b96e3fd1a9a311723b28
jwgilliam <ggsmoustache@gmail.com>
2020-05-06 09:45:32 -0500

    doing merges

--- Capstone/Startup.cs
+++ Capstone/Startup.cs
@@ -12,6 +12,7 @@ using Capstone.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Capstone.Models;
 
 namespace Capstone
 {
@@ -30,7 +31,7 @@ namespace Capstone
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
