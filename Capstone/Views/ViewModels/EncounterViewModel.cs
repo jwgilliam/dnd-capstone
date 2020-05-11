5b47b1c64cef96ad85e77aedd581074756814acc
jwgilliam <ggsmoustache@gmail.com>
2020-05-11 10:20:32 -0500

    added viewmodel

---
+++ Capstone/Views/ViewModels/EncounterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Views.ViewModels
+{
+    public class EncounterViewModel
+    {
+        public int Id { get; set; }
+        public Character Character { get; set; }
+        public List<Character> Characters { get; set; }
+        public Monster Monster { get; set; }
+        public List<Monster> Monsters { get; set; }
+        public ApplicationUser ApplicationUser {get; set;}
+    }
+}
